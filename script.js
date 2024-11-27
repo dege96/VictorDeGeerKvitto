e3a41ab20ddff2dd2196dbfe348cfec1434e2942
dege96 <victordegeer96@gmail.com>
2024-11-27 21:04:37 +0100

    25

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 const portfolio = document.querySelector(".portfolio");
-const KognitivetLink = document.getElementsByClassName("KognitivetLink");
-const VideosKognitivet = document.getElementById("VideosKognitivet");
-const BackArrow = document.querySelector(".back_arrow");
+const KognitivetVideoLink = document.getElementsByClassName("KognitivetVideoLink");
+const KognitivetMusicLink = document.getElementsByClassName("KognitivetMusicLink");
+const Kognitivet_Video_and_Music_Link = document.getElementsByClassName("Kognitivet_Video_and_Music_Link");
+const VideosKognitivetContainer = document.getElementById("VideosKognitivetContainer");
+const MusicKognitivetContainer = document.getElementById("MusicKognitivetContainer");
+const BackArrows = document.querySelectorAll(".back_arrow");
 
-BackArrow.addEventListener('click', (event) => {
-  console.log("BackArrow clicked");
-  VideosKognitivet.style.display = "none";
-  portfolio.style.display = "block";
-}); 
+BackArrows.forEach((BackArrow) => {
+  BackArrow.addEventListener('click', (event) => {
+    console.log("BackArrow clicked");
+    VideosKognitivetContainer.style.display = "none";
+    MusicKognitivetContainer.style.display = "none";
+    portfolio.style.display = "block";
+  });
+});
 
-for (let i = 0; i < KognitivetLink.length; i++) {
-  KognitivetLink[i].addEventListener('click', (event) => {
-    console.log("KognitivetLink clicked");
+for (let i = 0; i < KognitivetVideoLink.length; i++) {
+  KognitivetVideoLink[i].addEventListener('click', (event) => {
+    console.log("KognitivetVideoLink clicked");
     portfolio.style.display = "none";
-    VideosKognitivet.style.display = "flex";
+    VideosKognitivetContainer.style.display = "flex";
   });
 }
+
+for (let i = 0; i < KognitivetMusicLink.length; i++) {
+  KognitivetMusicLink[i].addEventListener('click', (event) => {
+    console.log("KognitivetMusicLink clicked");
+    portfolio.style.display = "none";
+    MusicKognitivetContainer.style.display = "flex";
+  });
+}
+
+for (let i = 0; i < Kognitivet_Video_and_Music_Link.length; i++) {
+  Kognitivet_Video_and_Music_Link[i].addEventListener('click', (event) => {
+    console.log("Kognitivet_Video_and_Music_Link clicked");
+    portfolio.style.display = "none";
+    MusicKognitivetContainer.style.display = "flex";
+    VideosKognitivetContainer.style.display = "flex";
+  });
+}
+
 
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text).then(() => {
